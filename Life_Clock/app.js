d486ede3b29818411afba8eb4a1d205d0d85de43
Anna Em <anna.yang@nyu.edu>
2024-09-25 18:57:29 +0800

    Add files via upload

---
+++ Life_Clock/app.js
@@ -0,0 +1,45 @@
+//1. Make sure the page loads first
+window.addEventListener('load', function () {
+    // console.log("Page has loaded");
+    
+    function ageCalculate(){
+    let inputDate = new Date(document.getElementById("date_input").value);
+    
+    let birthDate = String(inputDate.getDate()).padStart(2, '0'); // Ensure day is 2 digits, chatGPT
+    let birthMonth = String(inputDate.getMonth() + 1).padStart(2, '0'); // Ensure month is 2 digits, chatGPT
+    let birthYear = inputDate.getFullYear();
+
+    let url = `https://api.apiverve.com/v1/agecalculator?dob=${birthYear}-${birthMonth}-${birthDate}`;
+
+    //2. Request data
+    fetch(url, {
+        headers: {
+            'x-api-key': '66f2352f-620e-4969-8431-2bde66259e4f', // Replace with your actual API key
+            'Content-Type': 'application/json'
+        }
+    })
+        
+        //3. Then get the status of the request 
+        .then(response => response.json()) 
+ 
+        //4. Then access the data
+        .then(function (data) {
+            // console.log(data);
+            //5. Do something with the data
+            let weeksLived = data.data.age_weeks;
+            document.getElementById("weeksLived").textContent=weeksLived;
+
+            let weeksLeft = 4000 - weeksLived;
+            document.getElementById("weeksLeft").textContent=weeksLeft;
+
+            let lifeLived = (weeksLived/4000)*100;
+            document.getElementById("lifeLived").textContent=lifeLived;
+        })
+
+        .catch(error => {
+            console.log("Error! :" + error);
+        }); 
+    }
+
+    this.document.querySelector("button").addEventListener("click",ageCalculate);
+});
